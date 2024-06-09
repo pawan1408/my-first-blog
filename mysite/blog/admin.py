b9285e49a1a016717a38c03c8e2b7d8a62398b64
pawan1408 <pawang1408@gmail.com>
2024-06-09 11:56:49 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("title", "published_date", "created_date")
+
+admin.site.register(Post,MemberAdmin)
